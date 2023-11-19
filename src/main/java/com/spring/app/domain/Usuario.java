b103aa0eced85077e7b6a71f1a4f0c05e2582e56
alessandroscoelho <alessandrosocoelho@gmail.com>
2023-11-19 12:28:00 -0300

    primeiro commit app

---
+++ src/main/java/com/spring/app/domain/Usuario.java
@@ -0,0 +1,61 @@
+package com.spring.app.domain;
+
+import java.io.Serializable;
+import org.hibernate.validator.constraints.Length;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity(name = "TB_USUARIO")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotEmpty(message = "Campo NOME é obrigatório.")
+	private String nome;
+	
+	@NotEmpty(message = "Campo LOGIN é obrigatório")
+	private String login;
+	
+	@NotEmpty(message = "Campo SENHA é obrigatório")
+	@Length(min = 6, max = 8, message = "A senha deve conter entre 6 e 8 digitos.")
+	private String senha;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
