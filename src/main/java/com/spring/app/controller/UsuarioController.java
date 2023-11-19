2653d65d8ff7deb9e704aa4e5f1b4edcf74754c0
alessandroscoelho <alessandrosocoelho@gmail.com>
2023-11-19 13:13:59 -0300

    commit findAll

--- src/main/java/com/spring/app/controller/UsuarioController.java
+++ src/main/java/com/spring/app/controller/UsuarioController.java
@@ -8,18 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.spring.app.domain.Usuario;
 import com.spring.app.service.UsuarioService;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
 		Usuario obj = usuarioService.findById(id);
 		return ResponseEntity.ok().body(obj);
-		
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> findAll() {
+		List<Usuario> list = usuarioService.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/spring/app/service/UsuarioService.java
+++ src/main/java/com/spring/app/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.spring.app.service;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,10 @@ public class UsuarioService {
 	public Usuario findById(Integer id) {
 		Optional<Usuario> obj = usuarioRepository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundExcepetion("Objeto não encontrado! Id: "+id+", Tipo: "+ Usuario.class.getName()));
-		
 	}
+
+	public List<Usuario> findAll() {
+		return usuarioRepository.findAll();
+	}
+
 }
